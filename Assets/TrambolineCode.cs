7942cf9e528d44849440e357b9136ccd5f917e3b
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2025-04-08 17:54:26 +0300

    last

--- Assets/TrambolineCode.cs
+++ Assets/TrambolineCode.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class TrambolineCode : MonoBehaviour
 {
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "player")
